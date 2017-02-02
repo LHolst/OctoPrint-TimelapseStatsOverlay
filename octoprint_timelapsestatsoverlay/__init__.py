ab6d6e01072f86ecc80819f64fd213aaffdb8afc
Leonard Holst <leholst.lh@googlemail.com>
2017-02-02 01:30:00 +0100

    remove plugin from plugin name

--- octoprint_timelapsestatsoverlay/__init__.py
+++ octoprint_timelapsestatsoverlay/__init__.py
@@ -70,7 +70,7 @@ class TimelapseStatsOverlayPlugin(  EventHandlerPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "TimelapseStatsOverlay Plugin"
+__plugin_name__ = "TimelapseStatsOverlay"
 
 def __plugin_load__():
     global __plugin_implementation__
